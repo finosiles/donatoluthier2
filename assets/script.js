044c4fea764d07f9a07529299856b3fe0b88fa82
finosiles <rfinosiles@gmail.com>
2025-11-27 17:31:48 -0300

    reparando paginas

--- assets/script.js
+++ assets/script.js
@@ -1,6 +1,64 @@
 // Script para abrir/cerrar el menú en móvil
-    const toggle = document.getElementById('menu-toggle');
-    const navLinks = document.getElementById('nav-links');
-    toggle.addEventListener('click', () => {
-      navLinks.classList.toggle('active');
-    });
+
+
+
+
+const btn = document.getElementById("menuBtn");
+const menu = document.getElementById("mobileMenu");
+
+btn.addEventListener("click", () => {
+  menu.classList.toggle("hidden");
+});
+
+const slider = document.getElementById("slider");
+  const slides = slider.children;
+  const dots = document.querySelectorAll(".dot");
+  const prevBtn = document.getElementById("prevBtn");
+  const nextBtn = document.getElementById("nextBtn");
+
+  let index = 0;
+
+  function updateSlider() {
+    slider.style.transform = `translateX(-${index * 100}%)`;
+    dots.forEach(dot => dot.classList.remove("bg-white"));
+    dots[index].classList.add("bg-white");
+  }
+
+  function nextSlide() {
+    index = (index + 1) % slides.length;
+    updateSlider();
+  }
+
+  function prevSlide() {
+    index = (index - 1 + slides.length) % slides.length;
+    updateSlider();
+  }
+
+  // Auto Play
+  let autoplay = setInterval(nextSlide, 4000);
+
+  // Buttons Controls
+  nextBtn.onclick = () => {
+    nextSlide();
+    restartAutoplay();
+  };
+  prevBtn.onclick = () => {
+    prevSlide();
+    restartAutoplay();
+  };
+
+  // Dots Controls
+  dots.forEach((dot, i) => {
+    dot.onclick = () => {
+      index = i;
+      updateSlider();
+      restartAutoplay();
+    };
+  });
+
+  function restartAutoplay() {
+    clearInterval(autoplay);
+    autoplay = setInterval(nextSlide, 4000);
+  }
+
+  updateSlider();
